e1f6913dfebbaad95af57c592c47d668de2e8331
Dan W-B <d@nielwb.com>
2023-02-16 15:46:57 +0000

    Updated Velocity Calculator to not require a default value (uses first value twice)

--- frontend/lib/utilities/velocity_calculator.dart
+++ frontend/lib/utilities/velocity_calculator.dart
@@ -1,32 +1,32 @@
-class VelocityCalculator {
+class VelocityCalculator<T extends num> {
   final Stopwatch _sw = Stopwatch();
-  double _velocity;
-  num _prev;
-  final num _default;
+  double _velocity = 0;
+  T? _prev;
 
-  VelocityCalculator({required num defaultValue})
-      : _default = defaultValue,
-        _prev = defaultValue,
-        _velocity = 0 {
+  VelocityCalculator() {
     _sw.start();
   }
 
-  reset({num? value}) {
-    _prev = value ?? _default;
+  reset({T? value}) {
+    _prev = value;
     _sw
       ..reset()
       ..start();
   }
 
-  pushValue(num x) {
+  pushValue(T x) {
     const double oneMillion = 1000000;
     double secondsElapsed = (_sw.elapsedMicroseconds as double) / oneMillion;
-    _velocity = (x - _prev) / secondsElapsed;
+    if (secondsElapsed == 0 || _prev == null) {
+      _velocity = 0;
+    } else {
+      _velocity = (x - _prev!) / secondsElapsed;
+    }
     _prev = x;
     _sw
       ..reset()
       ..start();
   }
 
-  num get velocity => _velocity;
+  double get velocity => _velocity;
 }
