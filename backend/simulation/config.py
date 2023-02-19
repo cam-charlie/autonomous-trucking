054f8f4dba60213194e7b9c4136333d295a9ab1e
Sam Penny <33956017+sam1penny@users.noreply.github.com>
2023-02-19 21:18:30 +0000

    fix typing

--- backend/simulation/config.py
+++ backend/simulation/config.py
@@ -8,16 +8,8 @@ class Config:
 
     @property
     def MAX_ACCELERATION(self) -> float:
-<<<<<<< HEAD
         return float(self.data["globals"]["max_truck_acceleration"])
 
     @property
     def MAX_VELOCITY(self) -> float:
         return float(self.data["globals"]["max_truck_velocity"])
-=======
-        return self.data["globals"]["max_truck_acceleration"]
-
-    @property
-    def MAX_VELOCITY(self) -> float:
-        self.data["globals"]["max_truck_velocity"]
->>>>>>> e1aa09807a2b1b3c6a1d2a0dbd2cbfcf4986b2db
