79ffbc192fbabb1b0a5b0f705b7b887f197d73a4
Houwang123 <mark.haoxiang@gmail.com>
2023-02-19 22:25:21 +0000

    Floyd-Warshall routing

--- backend/simulation/realm/realm.py
+++ backend/simulation/realm/realm.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
+import math
 from typing import TYPE_CHECKING
 from .truck import Truck
 from .graph import Node, Road, Edge, Junction
 from .entity import Actor
 if TYPE_CHECKING:
     from ..config import Config
-    from typing import Dict
+    from typing import Dict, Tuple
 
 class Realm:
     def __init__(self, config: Config) -> None:
@@ -32,21 +33,34 @@ class Realm:
                     self.nodes[int(r["end_node_id"])],
                     r["length"]
                 )
-    
-        # TODO(mark) temp
-        for k,node in self.nodes.items():
-            if isinstance(node, Junction) and node.id==3:
-                node._routing_table[4] = 0
+        
+        # Run Floyd-Warshall
+        distance: Dict[Tuple[int,int],float] = {} # Distance
+        direction: Dict[Tuple[int,int],int] = {} # Next path to take
+        for i, edge in enumerate(self.edges.values()):
+            distance[(edge._start.id,edge._end.id)] = edge.cost
+            direction[(edge._start.id,edge._end.id)] = i
+        for node in self.nodes.values():
+            distance[(node.id,node.id)] = 0
+            direction[(node.id,node.id)] = -1
+        for k in self.nodes.values():
+            for s in self.nodes.values():
+                for d in self.nodes.values():
+                    if distance.get((s.id,d.id),math.inf) > distance.get((s.id,k.id),math.inf) + distance.get((k.id,d.id),math.inf):
+                        distance[(s.id,d.id)] = distance[(s.id,k.id)] + distance[(k.id,d.id)]
+                        direction[(s.id,d.id)] = direction[(s.id,k.id)]
+            # Put direction into routing table format
+        for s in self.nodes.values():
+            for d in self.nodes.values():
+                if (s.id,d.id) in direction and direction[(s.id,d.id)] != None:
+                    s._routing_table[d.id] = direction[(s.id,d.id)]
 
         # trucks
         self.trucks = {}
         for t in data["trucks"]:
             truck = Truck.from_json(t, config)
             self.trucks[truck.id] = truck
-            self.nodes[t['current_node']].entry(truck)       
-
-        # TODO(mark) temp  
-        self.trucks[0]._velocity = 1
+            self.nodes[t['current_node']].entry(truck)
 
         for truck in self.trucks.values():
             self.actors[truck.id] = truck
@@ -57,7 +71,7 @@ class Realm:
             if isinstance(edge,Actor):
                 self.actors[edge.id] = edge
 
-    def update(self, actions: Dict[int, float], dt: float =1/30) -> Dict:
+    def update(self, actions: Dict[int, float], dt: float =1/30) -> Dict[int, bool]:
         """
         Runs logic.
 
@@ -80,5 +94,5 @@ class Realm:
         for edge in self.edges.values():
             edge.update(dt)
 
-        #TODO(mark) completed trucks (finished desired route)
-        return {}
+        dones = {truck.id: truck.done() for truck in self.trucks.values()}
+        return dones
