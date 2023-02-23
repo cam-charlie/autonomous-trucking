a70aa582e698c9504c6d01afcae4844229a8a84d
phimaea <sophienovelli2012@gmail.com>
2023-02-23 11:45:22 +0000

    Slight changes to logic

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-from operator import ne
 import os
 import sys
 sys.path.append(os.getcwd())
@@ -24,36 +23,37 @@ if __name__ == '__main__':
                     truck = actor._storage[t]
                     if truck.done == False: #Truck is waiting to be released
                         next_road = env.realm.edges[truck.next_container_id]
-                        if type(next_road) is Road:
-                            first_car_pos = next_road._trucks[0].position * next_road._length
-                            if first_car_pos > car_size: #There is space on the road
-                                #Release this truck
-                                actions[actor.id] = float(truck.id)
+                        assert type(next_road) is Road
+                        first_car_pos = next_road._trucks[0].position * next_road._length
+                        if first_car_pos > car_size: #There is space on the road
+                            #Release this truck
+                            actions[actor.id] = float(truck.id)
 
-            elif type(actor) is Road:
-                for x in range(len(actor._trucks)):
-                    i = len(actor._trucks) - x - 1
-                    this_truck = actor._trucks[i]
-                    if x == 0: #no truck in front of it: eventually going into a node
+        for edge in env.realm.edges.values():
+            if type(edge) is Road:
+                for i in range(len(edge._trucks)-1,-1,-1):
+                    i = len(edge._trucks) - x - 1
+                    this_truck = edge._trucks[i]
+                    if i == len(edge._trucks)-1: #no truck in front of it: eventually going into a node
                         if this_truck.velocity < this_truck.config.MAX_VELOCITY:
                             #Accelerate
-                            actions[actor.id] = float(this_truck.config.MAX_ACCELERATION)
+                            actions[edge.id] = float(this_truck.config.MAX_ACCELERATION)
                     else:
-                        next_truck = actor._trucks[i+1]
+                        next_truck = edge._trucks[i+1]
                         #Generate stopping distance
                         u = this_truck.velocity
                         v = next_truck.velocity
                         a = this_truck.config.MAX_ACCELERATION * (-1)
                         relative_stopping_distance = ((v * v) - (u * u)) / (2 * a) #SUVAT
 
-                        distance = (next_truck.position - this_truck.position) * actor._length
+                        distance = (next_truck.position - this_truck.position) * edge._length
 
                         if distance < relative_stopping_distance:
                             #Too close! Deccelerate
-                            actions[actor.id] = float(this_truck.config.MAX_ACCELERATION * (-1))
+                            actions[edge.id] = float(this_truck.config.MAX_ACCELERATION * (-1))
                         elif distance > relative_stopping_distance and this_truck.velocity < this_truck.config.MAX_VELOCITY:
                             #There's space - accelerate
-                            actions[actor.id] = float(this_truck.config.MAX_ACCELERATION)
+                            actions[edge.id] = float(this_truck.config.MAX_ACCELERATION)
 
         #Actually do the step        
         env.step(actions)
