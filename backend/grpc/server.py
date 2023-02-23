81fa3ff195285f79e5aafefd5756167ef6f54398
Leon Elias Kollar <lek49@cam.ac.uk>
2023-02-23 12:24:43 +0000

    Make server pretty.

--- backend/grpc/server.py
+++ backend/grpc/server.py
@@ -1,17 +1,20 @@
+from __future__ import annotations
 from concurrent import futures
 import time
 import grpc
 import os
 import sys
 
-
 sys.path.append(os.getcwd())
 from simulation.env import Env
-from simulation.draw.visualiser import Visualiser
 
-import trucking_pb2 #as trucking_pb2
-import trucking_pb2_grpc #as trucking_pb2_grpc
-from typing import Any
+import trucking_pb2
+import trucking_pb2_grpc
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing import Any
+
 
 class PositionDataStreamerServicer(trucking_pb2_grpc.PositionDataStreamerServicer):
     def __init__(self) -> None:
@@ -20,21 +23,19 @@ class PositionDataStreamerServicer(trucking_pb2_grpc.PositionDataStreamerService
     def getPositionData(self, request: trucking_pb2.TimeDelta, context: Any) -> trucking_pb2.PositionDataStream:
         stream = []
 
-        # s = int(request.seconds)
-        s = 3
+        s = int(request.seconds)
 
         for curr_time in range(s * 30):
             trucks_at_time = []
+
             for (tid, t) in env.realm.trucks.items():
                 trucks_at_time.append(trucking_pb2.Truck(truck_id=tid, destination_id=t.destination, road_id=1, progress=t.position))
+
             trucksPosAtTime = trucking_pb2.TruckPositionsAtTime(trucks=trucks_at_time, time=(self.currTime + curr_time)/30)
             stream.append(trucksPosAtTime)
             env.step({})
         self.currTime += s * 30
         return trucking_pb2.PositionDataStream(trucks=stream)
-
-
-
 
 
 def serve() -> None:
@@ -52,6 +53,3 @@ if __name__ == '__main__':
     env.reset(sys.argv[1])
 
     serve()
-
-
-
