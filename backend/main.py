fa668c8031b7886f76565dfb2e1ede4d69f5ea53
phimaea <sophienovelli2012@gmail.com>
2023-02-23 01:21:42 +0000

    Type checking

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
                 for t in actor._storage:
                     truck = actor._storage[t]
                     if truck.done == False: #Truck is waiting to be released
-                        next_road = env.realm.edges[truck.next_container_id()]
+                        next_road = env.realm.edges[truck.next_container_id]
                         if type(next_road) is Road:
                             first_car_pos = next_road._trucks[0].position * next_road._length
                             if first_car_pos > car_size: #There is space on the road
@@ -35,14 +35,14 @@ if __name__ == '__main__':
                     i = len(actor._trucks) - x - 1
                     this_truck = actor._trucks[i]
                     if x == 0: #no truck in front of it: eventually going into a node
-                        if this_truck.velocity() < this_truck.config.MAX_VELOCITY:
+                        if this_truck.velocity < this_truck.config.MAX_VELOCITY:
                             #Accelerate
                             actions[actor.id] = this_truck.config.MAX_ACCELERATION
                     else:
                         next_truck = actor._trucks[i+1]
                         #Generate stopping distance
-                        u = this_truck.velocity()
-                        v = next_truck.velocity()
+                        u = this_truck.velocity
+                        v = next_truck.velocity
                         a = this_truck.config.MAX_ACCELERATION * (-1)
                         relative_stopping_distance = ((v * v) - (u * u)) / (2 * a) #SUVAT
 
@@ -50,10 +50,10 @@ if __name__ == '__main__':
 
                         if distance < relative_stopping_distance:
                             #Too close! Deccelerate
-                            actions[actor.id] = this_truck.config.MAX_ACCELERATION * (-1)
+                            actions[actor.id] = float(this_truck.config.MAX_ACCELERATION * (-1))
                         elif distance > relative_stopping_distance and this_truck.velocity() < this_truck.config.MAX_VELOCITY:
                             #There's space - accelerate
-                            actions[actor.id] = this_truck.config.MAX_ACCELERATION
+                            actions[actor.id] = float(this_truck.config.MAX_ACCELERATION)
 
         #Actually do the step        
         env.step(actions)
