eed2ca8439df14bcfbfc569088bfbe2af659fbe5
Sam Penny <33956017+sam1penny@users.noreply.github.com>
2023-03-02 21:06:46 +0000

    added sim time to visualisation

--- backend/simulation/draw/visualiser.py
+++ backend/simulation/draw/visualiser.py
@@ -4,6 +4,9 @@ from __future__ import annotations
 import pygame
 import sys
 from typing import TYPE_CHECKING
+from .utils import COMIC_SANS
+from simulation.config import Config
+
 if TYPE_CHECKING:
     from ..realm.realm import Realm
 
@@ -31,6 +34,7 @@ class Visualiser:
         self.screen.fill("white")
 
         self.draw_graph()
+        self.draw_params()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -45,3 +49,10 @@ class Visualiser:
 
         for edge in self.realm.edges.values():
             edge.draw(self.screen)
+
+    def draw_params(self) -> None:
+        text_surface = COMIC_SANS.render(str(round(Config.get_instance().SIM_TIME, 2)), False, (0, 0, 0))
+        #rect = pygame.Rect(100, 100, 120, 120)
+        #text_rect = text_surface.get_rect(center=rect.center)
+        text_rect = text_surface.get_rect(center=(50,50))
+        self.screen.blit(text_surface, text_rect)
