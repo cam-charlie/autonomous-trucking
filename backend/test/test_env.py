64215eea3e2b88fc9b16ff9c29c50132a3f535e5
Leon Elias Kollar <lek49@cam.ac.uk>
2023-03-04 18:09:06 +0000

    Update tests.

--- backend/test/test_env.py
+++ backend/test/test_env.py
@@ -8,16 +8,16 @@ from simulation.env import Env
 env = Env()
 class TestEnv(unittest.TestCase):
     def test_setup(self) -> None:
-        env.reset("../shared/example.json")
+        env.reset_from_path("../shared/example.json")
 
 
     def test_rollout_basic(self) -> None:
-        env.reset("test/test_json/test_1.json")
+        env.reset_from_path("test/test_json/test_1.json")
         for _ in range(128):
             env.step({10001: 10.0})
 
     def test_route_completion(self) -> None:
-        env.reset("test/test_json/test_2.json")
+        env.reset_from_path("test/test_json/test_2.json")
         self.assertListEqual(env.realm.trucks[10000]._route,[1,2,3])
         self.assertListEqual(env.realm.trucks[10001]._route,[2,3])
 
