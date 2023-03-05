563d16d2e4969f3b78402d2bebcddd8f27a8998b
Leon Elias Kollar <lek49@cam.ac.uk>
2023-03-05 21:29:53 +0000

    Attempt to reconnect rather than crashing in case server is not up.

--- frontend/lib/state/communication/client.dart
+++ frontend/lib/state/communication/client.dart
@@ -12,6 +12,8 @@ import 'dart:convert';
 void main() async{
   var json_string = await File("./example.json").readAsString();
 
+  json_string = '{"nodes":[{"type":"junction","id":30,"position":{"x":-100.0,"y":-100.0}},{"type":"junction","id":31,"position":{"x":100.0,"y":100.0}},{"type":"depot","id":32,"position":{"x":-50.0,"y":50.0}}],"roads":[{"id":20,"start_node_id":30,"end_node_id":31,"length":282.842712474619},{"id":21,"start_node_id":31,"end_node_id":30,"length":282.842712474619}],"trucks":[{"id":10,"current_node":30,"current_road":null,"current_position":0.0,"route":[31],"start_time":0}],"globals":{"max_truck_acceleration":0.68,"max_truck_velocity":10.0,"sim_time":0.0}}';
+
   await startFromConfig(json_string);
   print('hi');
   for (int i = 0; i < 220; ++i){
