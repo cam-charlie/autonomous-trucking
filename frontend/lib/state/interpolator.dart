63ba482706942fc0031697171344334177341c46
Will Pinkerton <4pinkwi@gmail.com>
2023-03-06 13:06:33 +0000

    Fix

--- frontend/lib/state/interpolator.dart
+++ frontend/lib/state/interpolator.dart
@@ -65,7 +65,7 @@ class _Interpolator {
 
     double fracDist = _truckFracDistCovered(start, end, t0, ti, t1, map);
 
-    if (_depoted.containsKey([RenderVehicleID(start.truckId)])) {
+    if (!_depoted.containsKey([RenderVehicleID(start.truckId)])) {
       if (fracDist == -1) {
         _depoted[RenderVehicleID(start.truckId)] = true;
       } else {
